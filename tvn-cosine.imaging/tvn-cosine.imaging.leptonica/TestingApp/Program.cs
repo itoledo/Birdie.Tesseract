58462efe804bc7ed7931dec83314970a19d09c9b
Theunis <Theunis@DEV-THEUNISV>
2017-06-06 14:41:03 +0200

    Test

--- tvn-cosine.imaging/tvn-cosine.imaging.leptonica/TestingApp/Program.cs
+++ tvn-cosine.imaging/tvn-cosine.imaging.leptonica/TestingApp/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Leptonica;
+using System; 
+using Tvn.Cosine.Imaging;
 
 namespace TestingApp
 {
     class Program
     {
+        static string file = @"C:\Temp\1.jpg";
+
         static void Main(string[] args)
         {
+            var pix = Pix.Read(file);
+            var pixWithBorder = Pix2.pixAddBorder(pix, 23, Color.Red);
+            pixWithBorder.Write(@"C:\Temp\pixWithBorder.jpg");
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
