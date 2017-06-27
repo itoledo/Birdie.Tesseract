76196ff929b4ea654008931c0f2521a70cbd0f1e
Theunis <Theunis@DEV-THEUNISV>
2017-06-27 08:05:22 +0200

    implemented getters to pix class definition

--- tvn-cosine.imaging/tvn-cosine.imaging.leptonica/tvn-cosine.imaging.leptonica/Definitions/Pix/Pix.cs
+++ tvn-cosine.imaging/tvn-cosine.imaging.leptonica/tvn-cosine.imaging.leptonica/Definitions/Pix/Pix.cs
@@ -1,4 +1,4 @@
-﻿using System; 
+﻿using System;
 
 namespace Leptonica
 {
@@ -22,7 +22,145 @@ namespace Leptonica
             }
             set
             {
-                this.pixSetWidth(value);
+                if (this.pixSetWidth(value))
+                {
+                    throw new Exception("An error occurred while trying to set the width.");
+                }
+            }
+        }
+         
+        public int Height
+        {
+            get
+            {
+                return this.pixGetHeight();
+            }
+            set
+            {
+                if (this.pixSetHeight(value))
+                {
+                    throw new Exception("An error occurred while trying to set the height.");
+                }
+            }
+        }
+
+        public int Depth
+        {
+            get
+            {
+                return this.pixGetDepth();
+            }
+            set
+            {
+                if (this.pixSetDepth(value))
+                {
+                    throw new Exception("An error occurred while trying to set the depth.");
+                }
+            }
+        }
+         
+        public int Spp
+        {
+            get
+            {
+                return this.pixGetSpp();
+            }
+            set
+            {
+                if (this.pixSetSpp(value))
+                {
+                    throw new Exception("An error occurred while trying to set the spp.");
+                }
+            }
+        }
+
+        public int Wpl
+        {
+            get
+            {
+                return this.pixGetWpl();
+            }
+            set
+            {
+                if (this.pixSetWpl(value))
+                {
+                    throw new Exception("An error occurred while trying to set the wpl.");
+                }
+            }
+        }
+
+        public int RefCount
+        {
+            get
+            {
+                return this.pixGetRefcount();
+            }
+            set
+            {
+                if (this.pixChangeRefcount(value))
+                {
+                    throw new Exception("An error occurred while trying to change the refcount.");
+                }
+            }
+        }
+
+        public int XRes
+        {
+            get
+            {
+                return this.pixGetXRes();
+            }
+            set
+            {
+                if (this.pixSetXRes(value))
+                {
+                    throw new Exception("An error occurred while trying to set the x res.");
+                }
+            }
+        }
+
+        public int YRes
+        {
+            get
+            {
+                return this.pixGetYRes();
+            }
+            set
+            {
+                if (this.pixSetYRes(value))
+                {
+                    throw new Exception("An error occurred while trying to set the y res.");
+                }
+            }
+        }
+
+        public int InputFormat
+        {
+            get
+            {
+                return this.pixGetInputFormat();
+            }
+            set
+            {
+                if (this.pixSetInputFormat(value))
+                {
+                    throw new Exception("An error occurred while trying to set the input format.");
+                }
+            }
+        }
+
+        public PixColormap Colormap
+        {
+            get
+            {
+                return this.pixGetColormap();
+            }
+            set
+            {
+                if (this.pixSetColormap(value))
+                {
+                    throw new Exception("An error occurred while trying to set the colormap.");
+                }
             }
         }
     }
