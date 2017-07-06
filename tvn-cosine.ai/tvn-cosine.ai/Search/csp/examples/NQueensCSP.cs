b40b0027a78d4a8f6faddeb5ac8c3818398e86ae
tvn-cosine <tvn-cosine>
2017-07-06 09:15:35 +0200

    completed aima csp examples

---
+++ tvn-cosine.ai/tvn-cosine.ai/Search/csp/examples/NQueensCSP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace tvn.cosine.ai.search.csp.examples
+{
+    public class NQueensCSP : CSP<Variable, int>
+    {
+        public NQueensCSP(int size)
+        {
+            for (int i = 0; i < size; i++)
+                addVariable(new Variable("Q" + (i + 1)));
+
+            List<int> values = new List<int>();
+            for (int val = 1; val <= size; val++)
+                values.Add(val);
+            Domain<int> positions = new Domain<int>(values);
+
+            foreach (Variable var in getVariables())
+                setDomain(var, positions);
+
+            for (int i = 0; i < size; i++)
+            {
+                Variable var1 = getVariables()[i];
+                for (int j = i + 1; j < size; j++)
+                {
+                    Variable var2 = getVariables()[j];
+                    addConstraint(new DiffNotEqualConstraint<Variable>(var1, var2, 0));
+                    addConstraint(new DiffNotEqualConstraint<Variable>(var1, var2, j - i));
+                }
+            }
+        }
+    }
+}
---
+++ tvn-cosine.ai/tvn-cosine.ai/Search/csp/examples/NotEqualConstraint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tvn.cosine.ai.search.csp.examples
+{
+    /**
+     * Represents a binary constraint which forbids equal values.
+     * 
+     * @author Ruediger Lunde
+     */
+    public class NotEqualConstraint<VAR, VAL> : Constraint<VAR, VAL>
+        where VAR : Variable
+    {
+        private VAR var1;
+        private VAR var2;
+        private List<VAR> scope;
+
+        public NotEqualConstraint(VAR var1, VAR var2)
+        {
+            this.var1 = var1;
+            this.var2 = var2;
+            scope = new List<VAR>(2);
+            scope.Add(var1);
+            scope.Add(var2);
+        }
+
+        public List<VAR> getScope()
+        {
+            return scope;
+        }
+
+        public bool isSatisfiedWith(Assignment<VAR, VAL> assignment)
+        {
+            Object value1 = assignment.getValue(var1);
+            return value1 == null || !value1.Equals(assignment.getValue(var2));
+        }
+    }
+
+}
