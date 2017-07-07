935e32fffecc0937fe598fcf595f547195cb1387
tvn-cosine <tvn-cosine>
2017-07-07 12:10:04 +0200

    added amai probability

---
+++ tvn-cosine.ai/tvn-cosine.ai/probability/Iterator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace tvn.cosine.ai.probability
+{
+    /**
+     * Interface to be implemented by an object/algorithm that wishes to iterate
+     * over the possible assignments for the random variables comprising this
+     * Factor.
+     * 
+     * @see Factor#iterateOver(Iterator)
+     * @see Factor#iterateOver(Iterator, AssignmentProposition...)
+     */
+    public interface Iterator<T>
+    {
+        /**
+         * Called for each possible assignment for the Random Variables
+         * comprising this Factor.
+         * 
+         * @param possibleAssignment
+         *            a possible assignment, &omega;, of variable/value pairs.
+         * @param value
+         *            the value associated with &omega;
+         */
+        void iterate(IDictionary<RandomVariable, T> possibleAssignment, double value);
+    }
+}
---
+++ tvn-cosine.ai/tvn-cosine.ai/probability/ProbabilityDistribution.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using tvn.cosine.ai.probability.proposition;
+
+namespace tvn.cosine.ai.probability
+{
+    /**
+     * Artificial Intelligence A Modern Approach (3rd Edition): page 487.<br>
+     * <br>
+     * A probability distribution is a function that assigns probabilities to events
+     * (sets of possible worlds).<br>
+     * <br>
+     * <b>Note:</b> This definition is slightly different than that given in AIMA3e
+     * pg. 487, which in this API corresponds to a CategoricalDistribution.
+     * 
+     * @see <a href="http://en.wikipedia.org/wiki/Probability_distribution"
+     *      >Probability Distribution</a>
+     * 
+     * @author Ciaran O'Reilly
+     */
+    public interface ProbabilityDistribution<T>
+    {
+        /**
+         * @return a consistent ordered Set (e.g. LinkedHashSet) of the random
+         *         variables this probability distribution is for.
+         */
+        ISet<RandomVariable> getFor();
+
+        /**
+         * 
+         * @param rv
+         *            the Random Variable to be checked.
+         * @return true if this Distribution is for the passed in Random Variable,
+         *         false otherwise.
+         */
+        bool contains(RandomVariable rv);
+
+        /**
+         * Get the value for the provided set of values for the random variables
+         * comprising the Distribution (ordering and size of each must equal and
+         * their domains must match).
+         * 
+         * @param eventValues
+         *            the values for the random variables comprising the
+         *            Distribution
+         * @return the value for the possible worlds associated with the assignments
+         *         for the random variables comprising the Distribution.
+         */
+        double getValue(IEnumerable<T> eventValues);
+
+        /**
+         * Get the value for the provided set of AssignmentPropositions for the
+         * random variables comprising the Distribution (size of each must equal and
+         * their random variables must match).
+         * 
+         * @param eventValues
+         *            the assignment propositions for the random variables
+         *            comprising the Distribution
+         * @return the value for the possible worlds associated with the assignments
+         *         for the random variables comprising the Distribution.
+         */
+        double getValue(IEnumerable<AssignmentProposition<T>> eventValues);
+    }
+
+}
