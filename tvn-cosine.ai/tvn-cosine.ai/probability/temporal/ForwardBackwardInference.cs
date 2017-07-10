84b5cf4437dad648384013914d80bc56bbeda844
tvn-cosine <tvn-cosine>
2017-07-10 07:49:26 +0200

    added aima probability temporal

---
+++ tvn-cosine.ai/tvn-cosine.ai/probability/temporal/ForwardBackwardInference.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using tvn.cosine.ai.probability.proposition;
+
+namespace tvn.cosine.ai.probability.temporal
+{
+    /**
+     * Artificial Intelligence A Modern Approach (3rd Edition): page 576.<br>
+     * <br>
+     * 
+     * Generic interface for calling different implementations of the
+     * forward-backward algorithm for smoothing: computing posterior probabilities
+     * of a sequence of states given a sequence of observations.
+     * 
+     * 
+     * @author Ciaran O'Reilly
+     * 
+     */
+    public interface ForwardBackwardInference<T> : ForwardStepInference<T>, BackwardStepInference<T>
+    {
+
+        /**
+         * The forward-backward algorithm for smoothing: computing posterior
+         * probabilities of a sequence of states given a sequence of observations.
+         * 
+         * @param ev
+         *            a vector of evidence values for steps 1,...,t
+         * @param prior
+         *            the prior distribution on the initial state,
+         *            <b>P</b>(X<sub>0</sub>)
+         * @return a vector of smoothed estimates for steps 1,...,t
+         */
+        IList<CategoricalDistribution<T>> forwardBackward(IList<IList<AssignmentProposition<T>>> ev, CategoricalDistribution<T> prior);
+    } 
+}
