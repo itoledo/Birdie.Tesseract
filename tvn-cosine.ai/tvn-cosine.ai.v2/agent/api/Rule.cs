e2f07b1dbe4108481cd7d0dc81fd3b840f4fa6b2
Theunis van Niekerk <theunis.vanniekerk@outlook.com>
2017-07-20 17:13:25 +0200

    v2

---
+++ tvn-cosine.ai/tvn-cosine.ai.v2/agent/api/Rule.cs
@@ -0,0 +1,16 @@
+﻿using tvn_cosine.ai.v2.common;
+
+namespace tvn_cosine.ai.v2.agent.api
+{
+    /// <summary>
+    /// A simple condition-action rule definition.
+    /// </summary>
+    /// <typeparam name="A">the action type that the rule triggers.</typeparam>
+    /// <typeparam name="S">the state type that the condition predicate tests.</typeparam>
+    public interface Rule<A, S>
+    {
+        Predicate<S> condition();
+
+        A action();
+    }
+}
---
+++ tvn-cosine.ai/tvn-cosine.ai.v2/common/Predicate.cs
@@ -0,0 +1,10 @@
+﻿namespace tvn_cosine.ai.v2.common
+{
+    /// <summary>
+    /// Simple predicate
+    /// </summary>
+    /// <typeparam name="T">The type of the input</typeparam>
+    /// <param name="input">The input</param>
+    /// <returns>boolean value</returns>
+    public delegate bool Predicate<T>(T input);
+}
