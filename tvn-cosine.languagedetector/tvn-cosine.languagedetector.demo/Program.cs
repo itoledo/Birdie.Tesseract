cb258893add3e2e3a8dc86d50029b0c177d046da
tvn-cosine <tvn-cosine>
2017-07-28 13:26:28 +0200

    demo

--- tvn-cosine.languagedetector/tvn-cosine.languagedetector.demo/Program.cs
+++ tvn-cosine.languagedetector/tvn-cosine.languagedetector.demo/Program.cs
@@ -8,8 +8,28 @@ namespace tvn_cosine.languagedetector.demo
 {
     class Program
     {
+        static string[] inputTexts = { @"Western Cape Premier Helen Zille is due to face a motion of no confidence in the provincial legislature following her infamous tweets about colonialism."
+                                    ,  @"Die polisie het Donderdagoggend die betogers wat strate in die Grassy Park-omgewing van Kaapstad versper het met waterkanonne, traanrook en rubberkoeëls uiteengejaag. Hulle het reeds van vroeg af oor swak dienslewering betoog."
+                                    ,  @"Ulahlwe ngelakubo owesilisa kwa-CC eMlazi, eningizimu yeTheku, okusolwa ukuthi ubezama ukudlwengula umfana oneminyaka engu-15 ubudala njengoba eshaywe wabulawa ngamalungu omphakathi ebusuku."
+                                    ,  @"Leia as Normas do Forum por favor!"};
+
         static void Main(string[] args)
         {
+            DetectorFactory.loadProfile("./profiles/");
+            Detector detector = DetectorFactory.create();
+            foreach (string input in inputTexts)
+            {
+                detector.append(input);
+                Console.WriteLine("Lang: {0}", detector.detect());
+                foreach (var prob in detector.getProbabilities())
+                {
+                    Console.WriteLine("Other: {0} at P({1})", prob.lang, prob.prob);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
         }
     }
 }
