d653d7a5a81491943e6b9a3e9b9fa61c951bf520
tvn-cosine <tvn-cosine>
2017-07-28 13:45:20 +0200

    fixes

--- tvn-cosine.languagedetector/tvn-cosine.languagedetector.demo/Program.cs
+++ tvn-cosine.languagedetector/tvn-cosine.languagedetector.demo/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
 
 namespace tvn_cosine.languagedetector.demo
 {
     class Program
     {
         static string[] inputTexts = { @"Western Cape Premier Helen Zille is due to face a motion of no confidence in the provincial legislature following her infamous tweets about colonialism."
-                                    ,  @"Die polisie het Donderdagoggend die betogers wat strate in die Grassy Park-omgewing van Kaapstad versper het met waterkanonne, traanrook en rubberkoeëls uiteengejaag. Hulle het reeds van vroeg af oor swak dienslewering betoog."
-                                    ,  @"Ulahlwe ngelakubo owesilisa kwa-CC eMlazi, eningizimu yeTheku, okusolwa ukuthi ubezama ukudlwengula umfana oneminyaka engu-15 ubudala njengoba eshaywe wabulawa ngamalungu omphakathi ebusuku."
-                                    ,  @"Leia as Normas do Forum por favor!"};
+                                     , @"Die polisie het Donderdagoggend die betogers wat strate in die Grassy Park-omgewing van Kaapstad versper het met waterkanonne, traanrook en rubberkoeëls uiteengejaag. Hulle het reeds van vroeg af oor swak dienslewering betoog."
+                                     , @"Ulahlwe ngelakubo owesilisa kwa-CC eMlazi, eningizimu yeTheku, okusolwa ukuthi ubezama ukudlwengula umfana oneminyaka engu-15 ubudala njengoba eshaywe wabulawa ngamalungu omphakathi ebusuku."
+                                     , @"Leia as Normas do Forum por favor!"};
 
         static void Main(string[] args)
         {
