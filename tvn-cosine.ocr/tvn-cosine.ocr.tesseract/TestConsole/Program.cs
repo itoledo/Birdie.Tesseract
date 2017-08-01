a53d512308a7cd54bc90b1e32a8f4d04491367e4
tvn-cosine <tvn-cosine>
2017-08-01 08:49:38 +0200

    added utf8 coversion

--- tvn-cosine.ocr/tvn-cosine.ocr.tesseract/TestConsole/Program.cs
+++ tvn-cosine.ocr/tvn-cosine.ocr.tesseract/TestConsole/Program.cs
@@ -1,10 +1,4 @@
-﻿using Leptonica;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using Tesseract;
 
 namespace TestConsole
@@ -13,14 +7,14 @@ namespace TestConsole
     {
         private const string tessdata = @"C:/Program Files (x86)/Tesseract-OCR/tessdata/";
         private const string language = "eng";
-        private const string image = @"C:\Temp\5.jpg";
+        private const string image = @"Test.png";
 
         static void Main(string[] args)
         { 
             string language = @"eng";
             using (var api = new TessBaseAPI(tessdata, language))
             {
-                api.Process(image, true);
+                api.Process(image);
                 string text = api.GetUTF8Text();
             }
 
